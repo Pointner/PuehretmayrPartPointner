f869e8ec910e47ff89fe3b8e9639c6fe2c3899f3
paula190099 <97699556+paula190099@users.noreply.github.com>
2022-06-22 12:54:18 +0200

    changes scaler

--- Scaler/Teachablemachinescaler/src/net/htlgkr/teachablemachine/scaler/ImageScaler.java
+++ Scaler/Teachablemachinescaler/src/net/htlgkr/teachablemachine/scaler/ImageScaler.java
@@ -10,18 +10,19 @@ import java.io.IOException;
 public class ImageScaler {
     public static void main(String[] args) throws IOException {
         ImageScaler m = new ImageScaler();
+        System.out.println("start");
 
         File[] filesfolder1 = new File("/Users/paula/Desktop/Teachable-Machine/Scaler/Teachablemachinescaler/pictures/").listFiles();
         for (File file : filesfolder1) {
             if (file.isDirectory())
                 for (File f : file.listFiles()) {
 
+                    System.out.println("Image found");
                     Image imagefolder1 = m.scaleImage(f);
-
-
                     try {
 
                         ImageIO.write((BufferedImage)imagefolder1,"jpeg",f);  // ignore returned boolean
+                        System.out.println("Image scaled");
                     } catch(IOException e) {
 
                     }
@@ -46,6 +47,7 @@ public class ImageScaler {
         JLabel jLabel = new JLabel();
         jLabel.setIcon(icon);
         frame.add(jLabel);
+            System.out.println("Image scaled 1");
 
         return image;
 
