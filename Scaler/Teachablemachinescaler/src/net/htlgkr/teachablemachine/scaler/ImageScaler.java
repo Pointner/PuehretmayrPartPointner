5a0490edea42c0dae2757b112f7097c8409399d3
paula190099 <97699556+paula190099@users.noreply.github.com>
2022-06-22 17:39:23 +0200

    multiple folder scaler

--- Scaler/Teachablemachinescaler/src/net/htlgkr/teachablemachine/scaler/ImageScaler.java
+++ Scaler/Teachablemachinescaler/src/net/htlgkr/teachablemachine/scaler/ImageScaler.java
@@ -8,6 +8,8 @@ import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
 import java.nio.Buffer;
+import java.nio.file.Paths;
+import java.util.Objects;
 
 public class ImageScaler {
     public static void main(String[] args) throws IOException {
@@ -15,52 +17,14 @@ public class ImageScaler {
         System.out.println("start");
         int i = 0;
 
-        m.scaleImage(new File("/Users/paula/Desktop/Teachable-Machine/Scaler/Teachablemachinescaler/pictures/scaledfolder1/macaca_nigra_self-portrait-3e0070aa19a7fe36e802253048411a38f14a79f8-s1100-c50.jpg"));
-
-       /* try {
-
-            BufferedImage bi = new BufferedImage(
-                    imagetry.getIconWidth(),
-                    imagetry.getIconHeight(),
-                    BufferedImage.TYPE_INT_RGB);
-            File outputfile = new File("image"+ ".jpg");
-            ImageIO.write(bi, "jpg", outputfile);
-            i++;
-        } catch(IOException e) {
-
-        }
-
-        */
-/*
-        File[] filesfolder1 = new File("/Users/paula/Desktop/Teachable-Machine/Scaler/Teachablemachinescaler/pictures/scaledfolder1/").listFiles();
-        for (File file : filesfolder1) {
-            if (file.isDirectory())
-                for (File f : file.listFiles()) {
-
-                    System.out.println("Image found");
-                    Image imagefolder1 = m.scaleImage(f);
-                    try {
-
-                        File outputfile = new File("image"+ i + ".jpg");
-                        ImageIO.write((BufferedImage)imagefolder1, "jpg", outputfile);
-                        i++;
-                    } catch(IOException e) {
-
-                    }
-
-                }
-        }
-
- */
-
-
+        m.scaleImages(Objects.requireNonNull(new File("pictures/folder1").listFiles()));
+        m.scaleImages(Objects.requireNonNull(new File("pictures/folder2").listFiles()));
 
 
     }
-        public void scaleImage(File f) throws IOException {
-            BufferedImage bufferedImage = ImageIO.read(f);
-
-            Image image = bufferedImage.getScaledInstance(500, 500, Image.SCALE_DEFAULT);
+    public void scaleImage(File f) throws IOException {
+        BufferedImage bufferedImage = ImageIO.read(f);
+        Image image = bufferedImage.getScaledInstance(500, 500, Image.SCALE_DEFAULT);
 
         ImageIcon icon = new ImageIcon(image);
         JFrame frame = new JFrame();
@@ -70,24 +34,36 @@ public class ImageScaler {
         JLabel jLabel = new JLabel();
         jLabel.setIcon(icon);
         frame.add(jLabel);
-            System.out.println("Image scaled 1");
+        System.out.println("Image scaled 1");
 
-            try {
+        try {
+            BufferedImage outputImage = new BufferedImage(frame.getWidth(), frame.getHeight(), BufferedImage.TYPE_INT_RGB);
+            outputImage.getGraphics().drawImage(image, 0, 0, null);
+            new File(f.getAbsolutePath().substring(0, f.getAbsolutePath().lastIndexOf("/")) + "-scale//").mkdirs();
+            File outputfile = new File(f.getAbsolutePath().substring(0, f.getAbsolutePath().lastIndexOf("/")) + "-scale/" + f.getName());
+            ImageIO.write(outputImage, "jpg", outputfile);
 
-                BufferedImage bi = new BufferedImage(
-                        icon.getIconWidth(),
-                        icon.getIconHeight(),
-                        BufferedImage.TYPE_INT_RGB);
-                File outputfile = new File("image"+ "test.jpg");
-                ImageIO.write(bi, "jpg", outputfile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-            } catch(IOException e) {
+    }
 
+    public void scaleImages(File[] files) {
+        for (File file : files) {
+            if (file.isDirectory()) {
+                System.out.println("Directory: " + file.getAbsolutePath());
+                scaleImages(Objects.requireNonNull(file.listFiles())); // Calls same method again.
+            } else {
+                try {
+                    scaleImage(file);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-
-
+        }
     }
 
 
 
-    }
+}
