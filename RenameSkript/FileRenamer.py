636699807b9511b048a9da2511c5072eb46d551b
Jonassaas1 <91061888+Jonassaas1@users.noreply.github.com>
2022-06-22 20:07:22 +0200

    Comment updated

--- RenameSkript/FileRenamer.py
+++ RenameSkript/FileRenamer.py
@@ -1,18 +1,17 @@
 import os
 import pathlib
 
-folder = 'C:/Schule/Github/Teachable-Machine/Scaler/Teachablemachinescaler/pictures/Orang-Utan Affe/'
+folder = 'Hier Folder Path der zu ändernden Files eingeben'
 count = 1
-# count increase by 1 in each iteration
-# iterate all files from a directory
+
 for file_name in os.listdir(folder):
-    # Construct old file name
+
     source = folder + file_name
 
-    # Adding the count to the new file name and extension
+
     destination = folder + "OrangUtan_" +str(count) + pathlib.Path(file_name).suffix
 
-    # Renaming the file
+
     os.rename(source, destination)
     #print(destination)
     count += 1
