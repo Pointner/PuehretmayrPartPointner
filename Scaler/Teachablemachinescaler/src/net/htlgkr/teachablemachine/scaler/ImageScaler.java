994de26d1e1f40834c55159c4387b45192a27b9c
paula190099 <97699556+paula190099@users.noreply.github.com>
2022-06-22 12:47:45 +0200

    adding folder for scaling

--- Scaler/Teachablemachinescaler/src/net/htlgkr/teachablemachine/scaler/ImageScaler.java
+++ Scaler/Teachablemachinescaler/src/net/htlgkr/teachablemachine/scaler/ImageScaler.java
@@ -9,12 +9,32 @@ import java.io.IOException;
 
 public class ImageScaler {
     public static void main(String[] args) throws IOException {
-
         ImageScaler m = new ImageScaler();
-        m.scaleImage();
+
+        File[] filesfolder1 = new File("/Users/paula/Desktop/Teachable-Machine/Scaler/Teachablemachinescaler/pictures/").listFiles();
+        for (File file : filesfolder1) {
+            if (file.isDirectory())
+                for (File f : file.listFiles()) {
+
+                    Image imagefolder1 = m.scaleImage(f);
+
+
+                    try {
+
+                        ImageIO.write((BufferedImage)imagefolder1,"jpeg",f);  // ignore returned boolean
+                    } catch(IOException e) {
+
+                    }
+
+                }
+        }
+
+
+
+
     }
-        public void scaleImage() throws IOException {
-            BufferedImage bufferedImage = ImageIO.read(new File("/Users/paula/Desktop/Teachable-Machine/Scaler/Teachablemachinescaler/pictures/monkey.jpeg"));
+        public Image scaleImage(File f) throws IOException {
+            BufferedImage bufferedImage = ImageIO.read(f);
 
             Image image = bufferedImage.getScaledInstance(500, 500, Image.SCALE_DEFAULT);
 
@@ -26,8 +46,10 @@ public class ImageScaler {
         JLabel jLabel = new JLabel();
         jLabel.setIcon(icon);
         frame.add(jLabel);
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        return image;
+
+
     }
 
 
