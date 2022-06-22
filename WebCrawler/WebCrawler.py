83b26ddcc75aa9c9131a9e40a44b2d7c98325ec3
Jonassaas1 <91061888+Jonassaas1@users.noreply.github.com>
2022-06-22 19:57:28 +0200

    Prototype deleted
    weil extra skript gemacht

--- WebCrawler/WebCrawler.py
+++ WebCrawler/WebCrawler.py
@@ -18,13 +18,7 @@ def search(query):
    
     gis = GoogleImagesSearch('AIzaSyB2wwkOu9uczr6l1Xx_9TbsH1AAiyVfjdw', 'b4b1b01e9779743b6')
     gis.search(search_params=_search_params, path_to_dir='path/'+query+'/')
-    i = 1
-    for path in pathlib.Path("Change des zu dem Pfad wo des bei dir liegt + query").iterdir():
-        old_extension = path.suffix
-        directory = path.parent
-        new_name = query + "_" + i + old_extension
-        path.rename(pathlib.Path(directory, new_name))
-        i += 1
+   
  
 
 if __name__ == '__main__':
