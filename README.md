e2ed885da6fde0b98edfbf92516ae5875c7f1ebd
Jonassaas1 <91061888+Jonassaas1@users.noreply.github.com>
2022-06-23 12:33:18 +0200

    some Changes

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Teachable-Machine
-Hallo Herr Welsch,
-Wenn Sie das hier lesen sind Sie ein Ehrenmann
+Hallo Herr Welsch, <br>
+Wenn Sie das hier lesen sind Sie ein Ehrenmann<br>
 Hier noch ein Link den Sie unbedingt sehen müssen:
 https://www.youtube.com/watch?v=dQw4w9WgXcQ
--- RenameSkript/FileRenamer.py
+++ RenameSkript/FileRenamer.py
@@ -1,19 +1,22 @@
 import os
 import pathlib
+def renamer():
+    folder = 'Hier Folder Path der zu ändernden Files eingeben'
+    count = 1
 
-folder = 'Hier Folder Path der zu ändernden Files eingeben'
-count = 1
+    for file_name in os.listdir(folder):
 
-for file_name in os.listdir(folder):
-
-    source = folder + file_name
-
-
-    destination = folder + "OrangUtan_" +str(count) + pathlib.Path(file_name).suffix
+        source = folder + file_name
 
 
-    os.rename(source, destination)
+        destination = folder + "OrangUtan_" +str(count) + pathlib.Path(file_name).suffix
+
+
+        os.rename(source, destination)
     #print(destination)
-    count += 1
+        count += 1
 
+
+if __name__ == '__main__':
+    renamer()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
